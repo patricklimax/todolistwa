f7e5541ac0db2b02e7c7e950c8487532132a9489
patricklimax <patricklimax@outlook.com.br>
2023-10-02 12:31:42 -0300

    melhoria no header

--- src/components/header/headerRight/index.tsx
+++ src/components/header/headerRight/index.tsx
@@ -1,21 +1,21 @@
 import './menuRight.css';
-import { IconBookmarkPlus, IconCoffee } from '@tabler/icons-react';
+import { IconBookmarkPlus } from '@tabler/icons-react';
 export const MenuRight = () => {
   return (
-    <nav className="flex items-center justify-center py-1 font-light">
+    <nav className="flex items-center justify-center p-1 font-light">
       <ul className="navRight">
         <li>
           <a href="https://patricklima.vercel.app/" target='_blank'>
             <IconBookmarkPlus stroke={2} size={22} color='#84cc16' />
-            <p className='hidden md:inline-block'>Mais</p>
+            <p className='font-medium'>Mais APP</p>
           </a>
         </li>
-        <li>
+        {/* <li>
           <a href="https://patricklima.vercel.app/" target='_blank'>
             <IconCoffee stroke={2} size={22} color='#84cc16' />
             <p className='hidden md:inline-block'>Café</p>
           </a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
